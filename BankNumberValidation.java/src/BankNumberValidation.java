19c9fa26f7dd6544567923e58d661556e80c2ace
Davi <dcarmo200010.dd@gmail.com>
2024-07-22 12:49:49 -0300

    updates

--- BankNumberValidation.java/src/BankNumberValidation.java
+++ BankNumberValidation.java/src/BankNumberValidation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VerificadorNumeroConta {
+public class BankNumberValidation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
